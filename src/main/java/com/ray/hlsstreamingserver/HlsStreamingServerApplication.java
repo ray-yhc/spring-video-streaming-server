e4ac12a3850b0c45f88421c730e069d92065a5d2
raycho <raycho521@yonsei.ac.kr>
2023-03-09 10:23:13 +0900

    Initial commit

---
+++ src/main/java/com/ray/hlsstreamingserver/HlsStreamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.ray.hlsstreamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlsStreamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HlsStreamingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ray/hlsstreamingserver/HlsStreamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ray.hlsstreamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlsStreamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
