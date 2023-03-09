fb88df80eea479bf1b699733445d061a1ccfabdb
raycho <raycho521@yonsei.ac.kr>
2023-03-09 12:58:09 +0900

    fix: add security, aws configurations

--- src/main/java/com/ray/hlsstreamingserver/HlsStreamingServerApplication.java
+++ src/main/java/com/ray/hlsstreamingserver/HlsStreamingServerApplication.java
@@ -2,10 +2,14 @@ package com.ray.hlsstreamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// todo : security 개발 후 제거하기
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
 public class HlsStreamingServerApplication {
-
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
     public static void main(String[] args) {
         SpringApplication.run(HlsStreamingServerApplication.class, args);
     }
